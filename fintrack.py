9675668a8ff72d18fa39fe346c6aa20bead6d942
Ritesh Kumar Singh <ritesh.singh05292005@gmail.com>
2026-08-11 17:56:43 +0530

    initial commit

---
+++ fintrack.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+data = {
+    "Date": pd.to_datetime([
+        "2026-01-01","2026-01-10",
+        "2026-02-01","2026-02-05",
+        "2026-03-01","2026-03-04","2026-03-08"
+        ]),
+    "Month":["Jan","Jan","Feb","Feb","Mar","Mar","Mar"],
+    "Category":["Rent","Food","Rent","Food","Rent","Food","Travel"],
+    "Amount":[15000,5000,15000,6000,15000,4000,3000],
+    "Payment_Mode":["Fixed","Variable","Fixed","Variable","Fixed","Variable","Variable"],
+    "Payment_Mode":["UPI","Cash","UPI","Cash","UPI","Cash","Cash"]
+}
+df=pd.DataFrame(data)
+print(df)
+# Total monthly expense
+print(df.groupby("Month")["Amount"].sum().reset_index())
+
